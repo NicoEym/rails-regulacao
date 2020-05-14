b6812b6a8ed4a0c081b9d9f41ef2218b57654a6c
nicolas <nicolas.eymenier@gmail.com>
2020-05-13 21:15:20 -0300

    Ok seed is read

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+  # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
@@ -8,33 +8,11 @@
 
 require 'csv'
 
-Governance.delete_all
+
+ArticleTopic.delete_all
 Topic.delete_all
 Article.delete_all
-
-def seed_governance(governance_array)
-
-  governance_array.each do |level|
-    Governance.create(title: governance)
-    puts "Creating #{governance}"
-  end
-end
-
-def seed_topic(topic_array)
-
-  topic_array.each do |topic|
-    Topic.create(name: topic)
-    puts "Creating #{topic}"
-  end
-end
-
-
-governance_seed = ["CVM", "Anbima"]
-
-seed_level(level_seed)
-
-
-
+Governance.delete_all
 
 csv_options = { col_sep: ';', quote_char: '"', headers: :first_row }
 filesarticles    = 'db/csv_repos/CVM.csv'
@@ -45,20 +23,20 @@ CSV.foreach(filesarticles, csv_options) do |row|
     Governance.create(title: row['title'])
     puts "Creating #{row['title']}"
     puts "#{Governance.find_by(title: row['title'])}"
-
   end
 
-  governance_id = Governance.find_by(name: row['title']).id
+  governance_id = Governance.find_by(title: row['title']).id
 
-  Article.create(title: governance_id, chapter: row['chapter'], section: row['section'], details: row['details'], number: row['number'] )
-  puts "Creating #{row['governance']} - #{row['chapter']} - #{row['section']}"
+  article = Article.create(governance_id: governance_id, chapter: row['chapter'], section: row['section'], details: row['details'], number: row['number'] )
+  puts "Creating Article #{row['number']} in #{row['governance']} - #{row['chapter']} - #{row['section']}"
 
   topics = row['topics'].split("/")
   topics.each do |topic|
     Topic.create(name: topic) if Topic.where(name: topic).empty?
     topic_id = Topic.find_by(name: topic).id
-    article_id = Article.find_by(chapter: row['chapter'], section: row['section'], number: row['number']).id
-    Article_topic.create(article_id: article_id, topic_id: topic_id)
+    puts "Creating Topic #{topic}"
+    ArticleTopic.create(article_id: article.id, topic_id: topic_id)
+    puts "Creating jointure article with topic #{topic}"
   end
 
 end
