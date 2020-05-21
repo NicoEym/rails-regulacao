d30a0e488235751cb7b7eea0956fcd02b4ea9a30
nicolas <nicolas.eymenier@gmail.com>
2020-05-21 18:15:39 -0300

    works well, just a few design to improve

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 //= require algolia/v3/algoliasearch.min
 //= require hogan
 import { initAutocomplete } from '../plugins/init_autocomplete';
+import { instantSearchTopics } from '../plugins/instant_search_topics.js';
 
-
-initAutocomplete();
+// initAutocomplete();
+instantSearchTopics();
---
+++ app/javascript/plugins/instant_search_topics.js
@@ -0,0 +1,53 @@
+const instantSearchTopics = () => {
+  var client = algoliasearch('OW017EKY1Y', '4bd2ad351fc1bb55a5ffd9c2fd98fffd');
+  var index = client.initIndex('dev_TOPIC');
+  const algoliaSearch = document.querySelector("#topics_input");
+
+
+  if (algoliaSearch === null) {return}
+  algoliaSearch.addEventListener('keyup', (event) => {
+    const drop = document.querySelector(".row");
+    // grid.innerHTML = "";
+    var keyword = event.currentTarget.value;
+    drop.innerHTML = "";
+    index.search(keyword, { hitsPerPage: 15, page: 0 })
+    .then(function searchDone(content) {
+      content.hits.forEach((hit) => {
+        console.log(addItineraryCard(hit));
+      })
+
+    })
+    .catch(function searchFailure(err) {
+    console.error(err);
+  });
+  });
+};
+
+
+
+
+const  addItineraryCard = (json) => {
+    const drop = document.querySelector(".row");
+  drop.insertAdjacentHTML('beforeend', html(json));
+};
+
+
+
+const html = (json) => {
+const new_html = `
+              <div class="col-xs-12 col-md-4">
+                  <a href="/topics/${json.id}/">
+                    <div class="card-topic" style="background-image: linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(https://images.unsplash.com/photo-1450101499163-c8848c66ca85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80)">
+
+                         <div class="card-topic_details">
+                          <h2 class="text-center">${json.name} </h2>
+                          <h6 class="text-center">${json.artigos} artigos relacionados...</h6>
+                        </div>
+                    </div>
+                  </div>
+                `;
+  return new_html;
+};
+
+
+export { instantSearchTopics };
