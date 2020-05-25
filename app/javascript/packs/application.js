41e79e208d30bae55ccd4288c18a771f06b20859
nicolas <nicolas.eymenier@gmail.com>
2020-05-25 10:07:37 -0300

    delete the line stylesheet pack tag in application.html.erb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 import "bootstrap";
 //= require algolia/v3/algoliasearch.min
-import { initAutocomplete } from '../plugins/init_autocomplete';
+// import { initAutocomplete } from '../plugins/init_autocomplete';
 import { instantSearchTopics } from '../plugins/instant_search_topics.js';
 import { instantSearchArticles } from '../plugins/instant_search_articles.js';
 
