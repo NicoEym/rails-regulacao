9e87ed2f3cafd040a317f7cd92878a7975d52c18
nicolas <nicolas.eymenier@gmail.com>
2020-12-23 11:31:07 -0300

    created the links to  the related articles for CVM558

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,22 +25,26 @@ class ArticlesController < ApplicationController
     # we get the related topics for this article
     @topics = @article.topics
     # we create two arrays to put the related articles fromCVM and from ANBIMA
-    @related_articles_CVM = []
+    @related_articles_CVM555 = []
+    @related_articles_CVM558 = []
     @related_articles_ANBIMA = []
 
     # for each related topic, we check if the articles are from CVM or from Anbima and put then in the array
     @topics.each do |topic|
       this_topic_articles = topic.articles
       this_topic_articles.each do |article|
-        if article.is_CVM?
-          @related_articles_CVM << article
+        if article.is_CVM555?
+          @related_articles_CVM555 << article
+        elsif article.is_CVM558?
+          @related_articles_CVM558 << article
         elsif article.is_ANBIMA?
           @related_articles_ANBIMA << article
         end
       end
     end
     # we send only the first 5 articles
-    @related_articles_CVM = @related_articles_CVM.uniq.first(5)
+    @related_articles_CVM555 = @related_articles_CVM555.uniq.first(5)
+    @related_articles_CVM558 = @related_articles_CVM558.uniq.first(5)
     @related_articles_ANBIMA = @related_articles_ANBIMA.uniq.first(5)
   end
 end
